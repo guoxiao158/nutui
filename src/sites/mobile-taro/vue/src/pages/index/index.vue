09b05410b2e9beb4811b725c19002fa5384ac0c9
richard1015 <51844712@qq.com>
2021-06-01 17:48:15 +0800

    feat: test demo

--- src/sites/mobile-taro/vue/src/pages/index/index.vue
+++ src/sites/mobile-taro/vue/src/pages/index/index.vue
@@ -7,7 +7,7 @@
     <nut-button type="danger">危险按钮</nut-button>
     <nut-button type="warning">警告按钮</nut-button>
     <nut-button type="success">成功按钮</nut-button>
-    <!-- <Demo></Demo> -->
+    <test>solt</test>
   </view>
 </template>
 
@@ -16,10 +16,12 @@ import { ref } from 'vue';
 import './index.scss';
 // import Button from '../../../../../../packages/__VUE/button/index.taro.vue';
 import Button from '../button/index.taro.vue';
-
+// import test from '../../../../test.vue';
+import test from './test.vue';
 export default {
   components: {
-    'nut-button': Button
+    'nut-button': Button,
+    test
   },
   setup() {
     const msg = ref('Hello world,Test NutUI');
---
+++ src/sites/mobile-taro/vue/src/pages/index/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <view class="test">
+    <text>{{ msg }}</text>
+    <slot></slot>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const msg = ref('test');
+    return {
+      msg
+    };
+  }
+};
+</script>
+
+<style>
+.test {
+  border: 1px solid red;
+}
+</style>
